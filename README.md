4dbf6afa73022b5b9eee94d0952dd0cf4454ceae
山田（朋） <yamada@inet.jeis.co.jp>
2017-05-30 09:56:44 +0900

    新規作成

---
+++ README.md
@@ -0,0 +1,4 @@
+# LargeDel
+Twitter Web Clientから画像を右クリック→保存すると拡張子が.jpg-largeになるので
+
+指定ディレクトリのなかの拡張子".xxx-large"を".xxx"に変換するだけのツール
---
+++ largeDel/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace largeDel
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("ディレクトリを指定してください>>>");
+            string folderPath = Console.ReadLine();
+
+            if (Directory.Exists(folderPath) != true)
+            {
+                Console.WriteLine("ディレクトリは存在しませんでした。");
+
+                return;
+            }
+
+            if (folderPath.EndsWith("\\") != true || folderPath.EndsWith("/") != true)
+            {
+                folderPath += "\\";
+            }
+            //指定ディレクトリ以下のファイルをすべて取得
+            IEnumerable<string> files = Directory.EnumerateFiles(folderPath, "*", SearchOption.TopDirectoryOnly);
+
+            //"-large"で終わるファイルから"-large"を削除する
+            foreach (string fname in files)
+            {
+                if (fname.EndsWith("-large") == true)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(fname);
+                    string extention = Path.GetExtension(fname);
+                    string originalExtention = extention.Replace("-large", string.Empty);
+                    try
+                    {
+                        File.Move(fname, folderPath + fileName + originalExtention);
+                        Console.WriteLine(fileName + extention + "\t->\t" + fileName + originalExtention);
+                    }
+                    catch (IOException)
+                    {
+                        Console.WriteLine(fileName + extention + "\t->\t変換後に同名のファイルがあるためスキップしました");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+
+            }
+            Console.WriteLine("終了しました");
+            Console.ReadLine();
+        }
+    }
+}
